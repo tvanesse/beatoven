36a6f7592c7401dad19415ae6e0eb3ad61209a71
Thomas Vanesse <vanessethomas@gmail.com>
2014-11-19 00:10:49 +0100

    Come on I'm not gonna write a meaningful comment for every fucking commit.

--- beatoven.py
+++ beatoven.py
@@ -20,6 +20,8 @@ from samples import Sample
 from gi.repository import Gtk
 from gi.repository.Gdk import keyval_name
 
+import os
+
 class BeatKey:
     """
     A key with a Sample assigned to it.
@@ -37,18 +39,41 @@ class Session:
     """
     A Beatoven session.
     """
-    def __init__(self, keyboard_layout="belgian"):
-        # Create a set of BeatKeys to cover the keyboard
-        #TODO
-        pass
+    def __init__(self, builder, keyboard_layout="belgian"):
+        # Get the Gtk builder
+        self.builder = builder
+        
+        # Create a set of BeatKeys to cover the keyboard and connect
+        # the buttons to their callbacks.
+        self.beatkeys = []
+        layout = open(os.getcwd() + "/layouts/" + keyboard_layout + ".txt", 'r')
+        for key in layout:
+            self.beatkeys.append(BeatKey(keyname=key))
+            button = self.builder.get_object("key_butt_" + key)
+            button.connect("clicked", on_key_button_clicked)
+        layout.close()
+        
+        # Connect the buttons to their callbac
+        
+        
+def on_key_button_clicked(widget, data=None):
+    # Open the sample chooser dialog
+    sample = sample_chooser.run()
+    if sample == -3:
+        print(sample_chooser.get_filename() + " selected")
+    print(widget.get_label().lower())
+#    for bk in beatkeys:
+#        if bk.key_button_id == widget.name:
+#            pass
 
 def key_pressed_handler(widget, event, data=None):
     key_pressed = keyval_name(event.keyval).lower()
-    for bk in beatkeys:
-        if bk.key_name == key_pressed:
-            # Highlight the corresponding button
-            # Play the sample
-            bk.sample.play_resume()
+    print(key_pressed)
+#    for bk in beatkeys:
+#        if bk.key_name == key_pressed:
+#            # Highlight the corresponding button
+#            # Play the sample
+#            bk.sample.play_resume()
 
 builder = Gtk.Builder()
 builder.add_from_file("gui/main.glade")
@@ -57,11 +82,14 @@ window1 = builder.get_object("window1")
 window1.connect("delete-event", Gtk.main_quit)
 window1.connect("key-press-event", key_pressed_handler)
 
-sample_chooser = builder.get_object("sample_chooser")
-loaded = sample_chooser.run()
+key_butt_f10 = builder.get_object("key_butt_f10")
+key_butt_f10.connect("clicked", on_key_button_clicked)
 
-if loaded == -3:
-    print(sample_chooser.get_filename() + " selected")
+sample_chooser = builder.get_object("sample_chooser")
+#loaded = sample_chooser.run()
+
+#if loaded == -3:
+#    print(sample_chooser.get_filename() + " selected")
 
 window1.show_all()
 Gtk.main()
